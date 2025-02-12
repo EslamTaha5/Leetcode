900db342a815dc741d2437f2febaf548fa6a556e
Eslam Taha <114341252+EslamTaha5@users.noreply.github.com>
2025-02-12 21:48:50 +0200

    Time: 24 ms (63.53%), Space: 63.9 MB (75.53%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maximumSum(vector<int>& nums) {
-        vector<int> mx1(100, -1), mx2(100, -1), freq(100);
-        int ans = -1;
+        int mx1[100]{}, mx2[100]{}, freq[100]{};
+        int ans = -1, num, sum;
         for(int i = 0; i < nums.size(); i++){
-            int x = nums[i], y = 0;
-            while(x){
-                y += x % 10;
-                x /= 10;
+            num = nums[i], sum = 0;
+            while(num){
+                sum += num % 10;
+                num /= 10;
             }
-            if(nums[i] > mx1[y]){
-                mx2[y] = mx1[y];
-                mx1[y] = nums[i];
-            }else if(nums[i] > mx2[y]){
-                mx2[y] = nums[i];
+            if(nums[i] > mx1[sum]){
+                mx2[sum] = mx1[sum];
+                mx1[sum] = nums[i];
+            }else if(nums[i] > mx2[sum]){
+                mx2[sum] = nums[i];
             }
-            freq[y]++;
-            if(freq[y] > 1) ans = max(ans, mx1[y] + mx2[y]);
+            freq[sum]++;
+            if(freq[sum] > 1) ans = max(ans, mx1[sum] + mx2[sum]);
         }
         return ans;
     }
