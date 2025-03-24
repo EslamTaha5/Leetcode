cfb780256816e32cada1c5a3f57589bcf964359d
Eslam Taha <114341252+EslamTaha5@users.noreply.github.com>
2025-03-24 16:00:47 +0200

    Time: 60 ms (84.69%), Space: 125.8 MB (99.49%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -6,12 +6,10 @@ public:
         sort(meetings.begin(), meetings.end());
         int ans = meetings[0][0] - 1;
         
-        int l = -1, r = meetings[0][1];
+        int r = -1;
         for(auto& it: meetings){
-            if(l == -1 || it[0] > r){
-                if(l != -1)ans += it[0] - r - 1;
-
-                l = it[0];
+            if(r == -1 || it[0] > r){
+                if(r != -1)ans += it[0] - r - 1;
                 r = it[1];
             }else{
                 r = max(r, it[1]);
