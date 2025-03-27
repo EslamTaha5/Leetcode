ccdab09e961ecd448f168b6639b6fe91be681cfe
Eslam Taha <114341252+EslamTaha5@users.noreply.github.com>
2025-03-27 10:13:55 +0200

    Time: 59 ms (22.13%), Space: 103 MB (12.3%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        map<int, int> pref, suff;
+        unordered_map<int, int> pref, suff;
         for(auto& it: nums)suff[it]++;
         for(int i = 0; i < nums.size() - 1; i++){
             pref[nums[i]]++;
