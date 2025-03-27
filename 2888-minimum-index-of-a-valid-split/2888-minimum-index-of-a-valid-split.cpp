f4218ca26cf523a9aff90fb806dc4c09bc7d1247
Eslam Taha <114341252+EslamTaha5@users.noreply.github.com>
2025-03-27 10:13:14 +0200

    Time: 98 ms (5.33%), Space: 103.8 MB (6.97%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        map<int, int> pref, suff;
+        for(auto& it: nums)suff[it]++;
+        for(int i = 0; i < nums.size() - 1; i++){
+            pref[nums[i]]++;
+            suff[nums[i]]--;
+            if(pref[nums[i]] * 2 > i + 1 &&
+                suff[nums[i]] * 2 > nums.size() - i - 1) return i;
+        }
+        return -1;
+    }
+};
