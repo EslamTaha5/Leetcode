4009cc8f30025d4da0fbd8a7b32bb1f674eceb33
Eslam Taha <114341252+EslamTaha5@users.noreply.github.com>
2025-05-06 14:45:07 +0300

    Time: 0 ms (100%), Space: 20.4 MB (81.05%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
+        int n = nums.size(), x = 0, y = 0;
+            x = (1 << 12) - 1;
+
         for(int i = 0; i < n; i++){
-            ans[i] = nums[nums[i]];
+            y = nums[i] & x;
+            nums[i] |= ((nums[y] & x) << 12);
         }
-        return ans;
+        for(auto& it: nums){
+            it >>= 12;
+        }
+        return nums;
     }
 };
